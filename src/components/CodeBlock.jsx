54925de7a21d2cdc9000f4dd3455fcd0a562bf0e
rogelcristobal <rogel.cristobal11@gmail.com>
2022-09-21 12:02:51 +0800

    added snackbar when copied

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,20 +1,57 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Paper, Typography, Snackbar, Alert } from "@mui/material";
+import Slide from '@mui/material/Slide';
+
 import {
   AiOutlineCopy,
   AiOutlineEyeInvisible,
   AiOutlineEye,
 } from "react-icons/ai";
-import { useState } from "react";
-const CodeBlock = ({ code, handleCopy }) => {
+import { useState, useRef } from "react";
+const CodeBlock = ({ code }) => {
   const [styleVisibility, setStyleVisibility] = useState(false);
+  const [open, setOpen] = useState(false);
+
   const handleShowStyles = () => {
     setStyleVisibility((prev) => (prev = !prev));
+    handleClose()
   };
+  const mergeTagsAndStyles = ({ tags, styles }) => {
+    if (!styles) {
+      return tags;
+    } else {
+      return styles + tags;
+    }
+  };
+  const handleCopy = () => {
+    navigator.clipboard.writeText(mergeTagsAndStyles(code));
+    setOpen(true);
+    
+  };
+
+  
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+  // mui transition prop
+  function SlideTransition(props) {
+    return <Slide {...props} direction="up" />;
+  }
 
   return (
     <Box className="h-full w-full relative box-border">
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose} direction="left" TransitionComponent={SlideTransition}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%","& .MuiAlert-icon":{
+          color:'white',
+          fontSize:'0.875rem'
+        } }} className="bg-blue-500 text-white text-xs flex items-center">
+          Copied to Clipboard!
+        </Alert>
+      </Snackbar>
       <SyntaxHighlighter
         wrapLongLines={true}
         wrapLines={true}
@@ -32,20 +69,24 @@ const CodeBlock = ({ code, handleCopy }) => {
         }}
       >
         {styleVisibility
-            ? code?.styles? `${code.styles}${code.tags}`:code.tags
+          ? code?.styles
+            ? `${code.styles}${code.tags}`
+            : code.tags
           : code.tags}
       </SyntaxHighlighter>
       <Box className="absolute bg-transparent top-3 right-3 flex items-center justify-around space-x-2">
-        <Paper
-          variant="outlined"
-          className={`p-2  bg-inherit border-neutral-600 flex items-center justify-center rounded-lg text-neutral-300 cursor-pointer gap-3 `}
-          onClick={handleShowStyles}
-        >
-          {styleVisibility ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
-          <Typography variant="body2" className="text-xs capitalize ">
-            {styleVisibility ? "view styles" : "hide styles"}
-          </Typography>
-        </Paper>
+        {code.styles && (
+          <Paper
+            variant="outlined"
+            className={`p-2  bg-inherit border-neutral-600 flex items-center justify-center rounded-lg text-neutral-300 cursor-pointer gap-3 `}
+            onClick={handleShowStyles}
+          >
+            {!styleVisibility ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+            <Typography variant="body2" className="text-xs capitalize ">
+              {!styleVisibility ? "view styles" : "hide styles"}
+            </Typography>
+          </Paper>
+        )}
         <Paper
           variant="outlined"
           className={`p-2 text-sm  bg-inherit border-neutral-600 flex items-center justify-center rounded-lg text-neutral-300 cursor-pointer gap-3 `}
--- src/pages/NormalListing.jsx
+++ src/pages/NormalListing.jsx
@@ -5,49 +5,10 @@ import PageSubTitle from "../components/PageSubTitle";
 import CodeBlock from "../components/CodeBlock";
 import CodeSnippetComponent from "../components/CodeSnippetComponent";
 import Texthighlight from "../components/Texthighlight";
+import template from "../template/template";
 const NormalListing = () => {
   const [tabState, setTabState] = useState(0);
-  const inTheBoxMarkup = {
-   tags:`
-<ul>
-  <li> item 1 </li>
-  <li> item 2 </li>
-  <li> item 3 </li>
-</ul>
-  `};
-
-  const specsMarkup = {
-  styles:`
-<style>
-  table.specTable {margin: 0 auto;}
-  table.specTable tr {vertical-align: top;}
-  table.specTable th, table.specTable td {width: 50%; padding-top: 1rem;}
-  table.specTable th {text-align: right; padding-right: 1rem;}
-  table.specTable td {padding-left:1rem;}
-  .item.spec h2 {font-weight: 500; text-align: center; border-bottom: solid 0.05rem black; padding-bottom: 0.5rem; color: #ff6600;}
-  .item.spec h3 {font-size: 2.5rem; margin: 2rem auto 0; width: fit-content; padding: 0 5rem 0.3rem 5rem; border-bottom: solid 0.05rem;}
-  .item.spec h4 {font-size: 1.6rem;font-weight: 500; text-align: center; text-transform: uppercase; margin-bottom: -0.5rem; text-decoration-line: underline;}
-</style>  
-  
-  `,
-  tags:`
-<table class="specTable">
-  <tbody>
-    <tr>
-      <th> Attribute 1 </th>
-      <td> Value 1 </td>
-    </tr>
-    <tr>
-      <th> Attribute 2 </th>
-      <td> Value 2 </td>
-    </tr>
-    <tr>
-      <th> Attribute 3 </th>
-      <td> Value 3 </td>
-    </tr>
-  </tbody>
-</table>`
-  };
+  const  {inTheBoxMarkup,specsMarkup} = template()
   return (
     <Box className="h-full w-full box-border flex items-start gap-3 rounded-lg p-0   ">
       <Box className="h-full overflow-auto w-full  box-border px-16  pt-16 pb-36">
