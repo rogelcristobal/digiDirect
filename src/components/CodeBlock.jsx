d62b92b954fc926cf4b06066c1e0b8fedc0edb92
rogelcristobal <rogel.cristobal11@gmail.com>
2022-10-03 15:07:35 +0800

    initialize images converter page design

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,24 +1,16 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
-import {
-  Box,
-  Paper,
-  Typography,
-  Snackbar,
-  Alert,
-  Divider 
-} from "@mui/material";
-import Tooltip,{ tooltipClasses } from '@mui/material/Tooltip' 
-import {DiCss3Full} from 'react-icons/di'
+import { Box, Paper, Dialog, Snackbar, Alert } from "@mui/material";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import { DiCss3Full } from "react-icons/di";
 import Slide from "@mui/material/Slide";
-import {MdEdit} from 'react-icons/md'
-import {FaClipboard} from 'react-icons/fa'
-import {BiCodeAlt} from 'react-icons/bi'
+import { MdEdit } from "react-icons/md";
+import { FaClipboard } from "react-icons/fa";
+import { BiCodeAlt } from "react-icons/bi";
 import { useState } from "react";
-import { googlecode } from "react-syntax-highlighter/dist/esm/styles/hljs";
-const CodeBlock = ({ code ,canEdit}) => {
+import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/hljs";
+const CodeBlock = ({ code, canEdit }) => {
   const [styleVisibility, setStyleVisibility] = useState(false);
   const [open, setOpen] = useState(false);
-
   const handleShowStyles = () => {
     setStyleVisibility((prev) => (prev = !prev));
     handleClose();
@@ -71,11 +63,11 @@ const CodeBlock = ({ code ,canEdit}) => {
         </Alert>
       </Snackbar>
       <SyntaxHighlighter
-      className="scrollbar-hide hover:scrollbar-default "
+        className="scrollbar-hide hover:scrollbar-default "
         wrapLongLines={true}
         wrapLines={true}
         language="css"
-        style={googlecode}
+        style={nightOwl}
         // showLineNumbers={true}
         customStyle={{
           paddingTop: "0.7rem",
@@ -87,8 +79,6 @@ const CodeBlock = ({ code ,canEdit}) => {
           boxSizing: "border-box",
           minHeight: !code ? "12rem" : "fit",
           maxHeight: "22rem",
-         
-          
         }}
       >
         {styleVisibility
@@ -97,40 +87,47 @@ const CodeBlock = ({ code ,canEdit}) => {
             : code.tags
           : code.tags}
       </SyntaxHighlighter>
-      <Box className="absolute bg-transparent right-0 -bottom-[4.5rem] box-border py-2 px-2 w-fit min-w-[10rem] flex items-center justify-end space-x-3 rounded-md">
-          <Paper
-            variant="contained"
-            className={`p-2 text-base  transition-all ease-in-out duration-300   flex items-center  rounded-lg justify-center  text-blue-500 bg-blue-100   cursor-pointer gap-3 `}
-            onClick={handleCopy}
-          >
-            <FaClipboard />
-           
-          </Paper>
+      <Box className="absolute bg-transparent  right-0 -bottom-[4rem] box-border py-2 px-2 w-fit min-w-[10rem] flex items-center justify-end space-x-3 rounded-md">
+        <Paper
+          variant="contained"
+          className={`p-2 text-base  transition-all ease-in-out duration-300   flex items-center  rounded-lg justify-center  text-blue-500 bg-blue-100   cursor-pointer gap-3 `}
+          onClick={handleCopy}
+        >
+          <FaClipboard />
+        </Paper>
+        {canEdit && (
           <Paper
             variant="contained"
             className={`p-2 text-base  transition-all ease-in-out duration-300   flex items-center  rounded-lg justify-center  text-blue-500 bg-blue-100   cursor-pointer gap-3 `}
             onClick={handleCopy}
           >
             <MdEdit />
-           
           </Paper>
-      {code.styles && (
-        <Paper
-          variant="contained"
-          className={`p-2    flex items-center  rounded-lg justify-center   transition-all ease-in-out duration-300   cursor-pointer gap-2 ${!styleVisibility?'text-blue-500 bg-blue-100':'text-white bg-blue-500'}`}
-          onClick={handleShowStyles}
-        >
-          {!styleVisibility ? <DiCss3Full  className="font-bold text-base"/> : <BiCodeAlt  className="font-bold text-base"/>}
-          {/* <Typography variant="body2" className=" text-xs font-medium  tracking-tight  ">
+        )}
+
+        {code.styles && (
+          <Paper
+            variant="contained"
+            className={`p-2    flex items-center  rounded-lg justify-center   transition-all ease-in-out duration-300   cursor-pointer gap-2 ${
+              !styleVisibility
+                ? "text-blue-500 bg-blue-100"
+                : "text-white bg-blue-500"
+            }`}
+            onClick={handleShowStyles}
+          >
+            {!styleVisibility ? (
+              <DiCss3Full className="font-bold text-base" />
+            ) : (
+              <BiCodeAlt className="font-bold text-base" />
+            )}
+            {/* <Typography variant="body2" className=" text-xs font-medium  tracking-tight  ">
             {!styleVisibility ? "View styles" : "Hide styles"}
           </Typography> */}
-        </Paper>
-      )}
-        
+          </Paper>
+        )}
       </Box>
     </Box>
   );
-
 };
 
 export default CodeBlock;
