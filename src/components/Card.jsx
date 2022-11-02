7bc7a91dcc6ccf11eb1820e04404e6debe91bf96
unknown <rogel.cristobal11@gmailcom>
2022-11-02 15:16:43 +0800

    fixed side nav

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import { HiOutlineArrowRight } from "react-icons/hi";
+import { HiArrowUpRight } from "react-icons/hi2";
 const Card = () => {
   return (
-    <Box className="h-40 w-full bg-[#3498ff] text-white p-4 flex  items-start cursor-pointer justify-between ">
-      <span className="capitalize text-lg "> get Started</span>
+    <Box className="h-24 w-72 bg-[#21252b] text-gray-500 p-6 flex  items-start cursor-pointer justify-between rounded-lg">
+      <span className="capitalize text-md font-normal"> get Started</span>
       <Box className="h-full w-[10%]  flex items-end justify-center text-2xl">
-        <HiOutlineArrowRight></HiOutlineArrowRight>
+        <HiArrowUpRight></HiArrowUpRight>
       </Box>
     </Box>
   );
