9a8e3a5b7fa305fca6ceb8abae7a282fb2c6dcf0
unknown <rogel.cristobal11@gmailcom>
2022-12-01 15:02:44 +0800

    added an arrow and a animatioin to linkBtn2

--- src/components/LinkBtn2.jsx
+++ src/components/LinkBtn2.jsx
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence,useAnimation } from "framer-motion";
+import {HiOutlineArrowUpRight} from 'react-icons/hi2'
 const LinkBtn2 = ({ title }) => {
   
- 
+  const [hoverState,setHoverState] = useState(false)
+
+  const arrowAnimation = useAnimation()
+  const handleHoverState =()=>{
+    setHoverState(!hoverState)
+    arrowAnimation.start({
+      y:!hoverState ? 0: 10,
+      opacity: !hoverState ? 1:0 ,
+      transition:{ease:'easeOut',duration:0.4}
+    })
+  }
+  
   return (
     <AnimatePresence>
       <motion.div
-      onClick={()=>alert('asd')}
-        className="text-[0.8rem]  cursor-none  py-2 inline-block relative font-ukraine-regular w-fit box-border hover-underline-animation whitespace-nowrap "
+        onHoverEnd={handleHoverState}
+        onHoverStart={handleHoverState}
+        className="text-[0.8rem]  cursor-none space-x-3 py-1 inline-block relative font-ukraine-regular flex items-center w-fit box-border hover-underline-animation whitespace-nowrap "
       >
+        <span>
+
         {title}
+        </span>
+        <motion.div  animate={arrowAnimation} initial={{ opacity:0 ,y : 5}}>
+          <HiOutlineArrowUpRight className=" text-lg"/>
+        </motion.div>
+        
       </motion.div>
     </AnimatePresence>
   );
